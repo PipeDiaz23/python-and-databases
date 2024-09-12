6ef48e93fd3ddbb2e72dfeef3acb1d15d6600517
Daniel Diaz <diazalzated2304@gmail.com>
2024-09-12 01:52:45 -0500

    ultimo commit

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 import streamlit as st
 import pandas as pd
+from concesionario import insert_data_bulk
 
-def _extract_students_from_excel(excel_file):
-        """Extracts student information from the provided Excel file."""
-        try:
-            df = pd.read_excel(excel_file)
-        except Exception as e:
-            st.write(f"Error reading the Excel file: {e}")
-            return []
-        st.write(df)
+def _extract_data_from_excel(excel_file):
+    try:
+        df = pd.read_excel(excel_file)
+        return df
+    except Exception as e:
+        st.write(f"Error reading the Excel file: {e}")
+        return pd.DataFrame()
 
+st.title("Upload two Excel files to combine")
 
-st.title("Upload students attendance list Excel file")
+uploaded_file_1 = st.file_uploader("Upload first Excel file", type=["xls", "xlsx"], key="file1")
 
-uploaded_file = st.file_uploader("Attendance list Excel file", type=["xls", "xlsx"])
+uploaded_file_2 = st.file_uploader("Upload second Excel file", type=["xls", "xlsx"], key="file2")
 
-if uploaded_file is not None:
-    st.write("file was uploaded successfully.")
-    _extract_students_from_excel(uploaded_file)
+if uploaded_file_1 is not None and uploaded_file_2 is not None:
+    st.write("Both files uploaded successfully.")
+    
+    df1 = _extract_data_from_excel(uploaded_file_1)
+    st.write(df1)
+    df2 = _extract_data_from_excel(uploaded_file_2)
+    st.write(df2)
+
+    
+    if not df1.empty and not df2.empty:
+        combined_df = pd.merge(df1, df2, on='ProductID', how='inner')
+        
+        st.write("Combined DataFrame:")
+        st.write(combined_df)
+        
+        insert_data_bulk(combined_df)
+        
+    else:
+        st.write("One or both files are empty or invalid.")
